14e10b18e2b845e34ba105ab517b78a9ac725cdf
Emma :) <99496596+kodukass@users.noreply.github.com>
2022-12-08 11:35:16 +0200

    8.12

--- TARge21Shop1/Controllers/SpaceshipsController.cs
+++ TARge21Shop1/Controllers/SpaceshipsController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TARge21Shop.Core.Domain.Spaceship;
+using TARge21Shop.Core.Dto;
+using TARge21Shop.Core.ServiceInterface;
 using TARge21Shop.Data;
+using TARge21Shop.Models.Spaceship;
 using TARge21Shop1.Models.Spaceship;
 
 namespace TARge21Shop.Controllers
@@ -7,13 +11,16 @@ namespace TARge21Shop.Controllers
     public class SpaceshipsController : Controller
     {
         private readonly TARge21ShopContext _context;
+        private readonly ISpaceshipsServices _spaceshipsServices;
 
         public SpaceshipsController
             (
-                TARge21ShopContext context
+                TARge21ShopContext context,
+                ISpaceshipsServices spaceshipsServices
             )
         {
-        _context = context;
+            _context = context;
+            _spaceshipsServices = spaceshipsServices;
         }
 
         public IActionResult Index()
@@ -31,10 +38,103 @@ namespace TARge21Shop.Controllers
             return View();
         }
 
+        [HttpGet]
         public IActionResult Add()
         {
+            SpaceshipEditViewModel spaceship = new SpaceshipEditViewModel();
 
-            return View("Edit");
+            return View("Edit", spaceship);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(SpaceshipEditViewModel vm)
+        {
+            var dto = new SpaceshipDto()
+            {
+                Id = vm.Id,
+                Name = vm.Name,
+                Type = vm.Type,
+                Crew = vm.Crew,
+                Passangers = vm.Passangers,
+                CargoWeight = vm.CargoWeight,
+                FullTripsCount = vm.FullTripsCount,
+                MaintenanceCount = vm.MaintenanceCount,
+                LastMaintenance = vm.LastMaintenance,
+                EnginePower = vm.EnginePower,
+                MaidenLaunch = vm.MaidenLaunch,
+                BuiltDate = vm.BuiltDate,
+                CreatedAt = vm.CreatedAt,
+                ModifiedAt = vm.ModifiedAt
+            };
+
+            var result = await _spaceshipsServices.Add(dto);
+
+            if (result == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            return RedirectToAction(nameof(Index), vm);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var spaceship = await _spaceshipsServices.GetUpdate(id);
+
+            if (spaceship == null)
+            {
+                return NotFound();
+            }
+
+            var dto = new SpaceshipEditViewModel()
+            {
+                Id = spaceship.Id,
+                Name = spaceship.Name,
+                Type = spaceship.Type,
+                Crew = spaceship.Crew,
+                Passangers = spaceship.Passangers,
+                CargoWeight = spaceship.CargoWeight,
+                FullTripsCount = spaceship.FullTripsCount,
+                MaintenanceCount = spaceship.MaintenanceCount,
+                LastMaintenance = spaceship.LastMaintenance,
+                EnginePower = spaceship.EnginePower,
+                MaidenLaunch = spaceship.MaidenLaunch,
+                BuiltDate = spaceship.BuiltDate,
+                CreatedAt = spaceship.CreatedAt,
+                ModifiedAt = spaceship.ModifiedAt
+            };
+            return View();           
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(SpaceshipEditViewModel vm)
+        {
+            var dto = new SpaceshipDto()
+            {
+                Id = vm.Id,
+                Name = vm.Name,
+                Type = vm.Type,
+                Crew = vm.Crew,
+                Passangers = vm.Passangers,
+                CargoWeight = vm.CargoWeight,
+                FullTripsCount = vm.FullTripsCount,
+                MaintenanceCount = vm.MaintenanceCount,
+                LastMaintenance = vm.LastMaintenance,
+                EnginePower = vm.EnginePower,
+                MaidenLaunch = vm.MaidenLaunch,
+                BuiltDate = vm.BuiltDate,
+                CreatedAt = vm.CreatedAt,
+                ModifiedAt = vm.ModifiedAt
+            };
+
+            var result = await _spaceshipsServices.Update(dto);
+
+            if (result == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            return RedirectToAction(nameof(Index), vm);
         }
     }
 }
